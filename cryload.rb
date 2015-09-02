58dbd69cdbc2eef90e427c31c5f1ca2122e5a93b
Sdogruyol <dogruyolserdar@gmail.com>
2015-09-02 22:38:50 +0300

    Updated tap

--- cryload.rb
+++ cryload.rb
@@ -1,7 +1,6 @@
 class Cryload < Formula
-  homepage ""
-  url ""
-  sha1 ""
+  homepage "https://github.com/Sdogruyol/cryload"
+  url "https://github.com/Sdogruyol/homebrew-cryload/archive/v0.0.2.zip"
 
   def install
   	bin.install "cryload"
